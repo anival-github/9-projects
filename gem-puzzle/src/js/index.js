38f2be6dabffba4279b938e4695b76243c6d9713
Valentin Anikin <anival-94@mail.ru>
2020-11-09 22:40:08 +0300

    feat: add shift puzzle function

--- gem-puzzle/src/js/index.js
+++ gem-puzzle/src/js/index.js
@@ -6,7 +6,9 @@ const gemPuzzle = {
     elements: {
         main: null,
         info: null,
+
         field: null,
+
         puzzlesContainer: null,
         puzzles: null,
         time: null,
@@ -18,10 +20,13 @@ const gemPuzzle = {
     },
 
     properties: {
-        paused: false,
-        layout: "3*3",
-        soundOn: true,
+        layout: "4*4",
+        puzzlesNumber: 0,
         currentField: null,
+        gridTemplateArea: null,
+
+        paused: false,
+        soundOn: true,
         completed: false,
         scoreShown: false,
         scores: [],
@@ -87,7 +92,7 @@ const gemPuzzle = {
         game.appendChild(gameWrapper)
         gameWrapper.appendChild(this.elements.field)
         this.elements.field.appendChild(this.elements.puzzlesContainer)
-        this.elements.puzzlesContainer.appendChild(this._createPuzzles())
+        this.elements.puzzlesContainer.appendChild(this._createPuzzles())  // ФУНКЦИЯ ПО ДОБАВЛЕНИЮ ПАЗЛОВ
 
         this.elements.puzzles = this.elements.puzzlesContainer.querySelectorAll('.puzzles__item')
 
@@ -119,7 +124,7 @@ const gemPuzzle = {
         this.elements.main.appendChild(this.elements.menu)
         this.elements.menu.appendChild(menuWrapper)
         menuWrapper.appendChild(navigation)
-        navigation.appendChild(this._createButtons())
+        navigation.appendChild(this._createButtons())  // ФУНКЦИЯ ПО ДОБАВЛЕНИЮ КНОПОК
 
         // Create block Message
         const message = document.createElement('section')
@@ -140,30 +145,28 @@ const gemPuzzle = {
     _createPuzzles() {
         const fragment = document.createDocumentFragment();
 
-        let puzzlesNumer
-
         switch (this.properties.layout) {
             case "4*4":
-                puzzlesNumer = 4 * 4
+                this.properties.puzzlesNumber = 4 * 4
                 break;
             case "5*5":
-                puzzlesNumer = 5 * 5
+                this.properties.puzzlesNumber = 5 * 5
                 break;
             case "6*6":
-                puzzlesNumer = 6 * 6
+                this.properties.puzzlesNumber = 6 * 6
                 break;
             case "7*7":
-                puzzlesNumer = 7 * 7
+                this.properties.puzzlesNumber = 7 * 7
                 break;
             case "8*8":
-                puzzlesNumer = 8 * 8
+                this.properties.puzzlesNumber = 8 * 8
                 break;
             default: //case 3*3
-                puzzlesNumer = 3 * 3
+                this.properties.puzzlesNumber = 3 * 3
                 break;
         }
 
-        for (let i = 0; i < puzzlesNumer; i += 1) {
+        for (let i = 0; i < this.properties.puzzlesNumber; i += 1) {
             let puzzleElement = document.createElement('div')
             puzzleElement.classList.add(`puzzle-${i}`)
             puzzleElement.setAttribute('id', `${i}`)
@@ -171,16 +174,55 @@ const gemPuzzle = {
             if (i === 0) {
                 puzzleElement.classList.add('puzzles__item', 'puzzles__item--empty')
             } else {
-                puzzleElement.classList.add('puzzles__item', 'puzzles__item--filled',  'puzzles__item--inactive')
+                puzzleElement.classList.add('puzzles__item', 'puzzles__item--filled', 'puzzles__item--inactive')
                 puzzleElement.innerText = i
+                puzzleElement.style = `grid-area: p${i};`
                 puzzleElement.addEventListener('click', (e) => {
-                    this._shiftPuzzle()
+                    this._shiftPuzzle(e)
                     this._playSound()
                 })
             }
             fragment.appendChild(puzzleElement)
         }
+
+        // Create grid styles
+        function createMatrix(rows) {
+            let arr = [];
+            let counter = 0;
+            for (var i = 0; i < rows; i++) {
+                arr[i] = [];
+                for (let j = 0; j < rows; j++) {
+                    arr[i][j] = `p${counter}`
+                    counter += 1
+                }
+            }
+            return arr;
+        }
+
+        this.properties.currentField = createMatrix(Math.sqrt(this.properties.puzzlesNumber))
+        this._mixPuzzles()
+        this._createGridArea()
+        this._addGridToPuzzleContainer()
         return fragment
+    },
+
+    _mixPuzzles(){
+        console.log(this.properties.currentField);
+        console.log(Math.random() * 16);
+
+    },
+
+    // Create GRID AREA TO BE INCLUDED IN CSS STYLES
+    _createGridArea() {
+        let arrayForGrid = this.properties.currentField.map(element => element.join(' ')).join('" "')
+        this.properties.gridTemplateArea = `"${arrayForGrid}"`
+    },
+
+    // CHANGE PUZZLE CONTAINER STYLE by including grid area
+    _addGridToPuzzleContainer() {
+        this.elements.puzzlesContainer.style =
+            `grid-template-columns: repeat(${Math.sqrt(this.properties.puzzlesNumber)}, 1fr);
+            grid-template-areas: ${this.properties.gridTemplateArea};`
     },
 
     _createButtons() {
@@ -227,7 +269,7 @@ const gemPuzzle = {
                     buttonElement.addEventListener('click', (e) => {
                         this._toggleSound()
                         this._playSound()
-                    } )
+                    })
                     break;
                 case 'Pause':
                     buttonElement.setAttribute('id', 'pause')
@@ -235,7 +277,7 @@ const gemPuzzle = {
                     buttonElement.addEventListener('click', (e) => {
                         this._togglePause()
                         this._playSound()
-                    } )
+                    })
                     break;
                 case 'Change Image':
                     buttonElement.setAttribute('id', 'change-image')
@@ -244,7 +286,7 @@ const gemPuzzle = {
                         this._changeImage()
                         this._pressNewGame()
                         this._playSound()
-                    } )
+                    })
                     break;
             }
 
@@ -264,8 +306,61 @@ const gemPuzzle = {
 
     },
 
-    _shiftPuzzle() {
+    _shiftPuzzle(e) {
+        // Reciece the current puzzle coordinates within the grid field
+        const puzzleCoordinates = this._getPuzzleCoordinates(e.target.id, this.properties.currentField)
+        const x = puzzleCoordinates[1]
+        const y = puzzleCoordinates[0]
 
+        // Identify coordinates of Up, Dowm, Left and Right puzzles in the currentField (grid)
+        // If so - identify the grid index of Up, Dowm, Left and Right puzzles
+        const puzzleUpCoordinates = [y - 1, x]
+        let puzzleUpGridIndex
+        if (y - 1 >= 0) {
+            puzzleUpGridIndex = this.properties.currentField[y - 1][x];
+        }
+
+        const puzzleDownCoordinates = [y + 1, x]
+        let puzzleDownGridIndex
+        if (y + 1 <= this.properties.currentField.length - 1) {
+            puzzleDownGridIndex = this.properties.currentField[y + 1][x]
+        }
+
+        const puzzleRightCoordinates = [y, x + 1]
+        let puzzleRightGridIndex
+        if (x + 1 <= this.properties.currentField[y].length - 1) {
+            puzzleRightGridIndex = this.properties.currentField[y][x + 1]
+        }
+
+        const puzzleLeftCoordinates = [y, x - 1]
+        let puzzleLeftGridIndex
+        if (x - 1 >= 0) {
+            puzzleLeftGridIndex = this.properties.currentField[y][x - 1]
+        }
+
+        // If Up, Dowm, Left or Right puzzles have grid index p0 - change it with current puzzle
+        if (puzzleUpGridIndex === 'p0') {
+            this.properties.currentField[y - 1][x] = `p${e.target.id}`
+            this.properties.currentField[y][x] = `p0`
+        }
+        if (puzzleDownGridIndex === 'p0') {
+            this.properties.currentField[y + 1][x] = `p${e.target.id}`
+            this.properties.currentField[y][x] = `p0`
+        }
+        if (puzzleRightGridIndex === 'p0') {
+            this.properties.currentField[y][x + 1] = `p${e.target.id}`
+            this.properties.currentField[y][x] = `p0`
+        }
+        if (puzzleLeftGridIndex === 'p0') {
+            this.properties.currentField[y][x - 1] = `p${e.target.id}`
+            this.properties.currentField[y][x] = `p0`
+        }
+
+
+        console.log(e.target);
+
+        this._createGridArea()
+        this._addGridToPuzzleContainer()
     },
 
     _endGame() {
@@ -302,25 +397,9 @@ const gemPuzzle = {
         //Change the layout
         this.elements.puzzlesContainer.innerHTML = ''
         this.elements.puzzlesContainer.appendChild(this._createPuzzles())
-
-        document.styleSheets[1].replace()
-
-        console.log(document.styleSheets[1]);
-
-
-
-        console.log(`function chenge layout started`)
-
-        // const currentLayout = document.querySelector('.puzzles')
-
-        // console.log(
-        //     currentLayout.classList
-        //     );
-
-
     },
 
-    _changeImage(){
+    _changeImage() {
 
     },
 
@@ -341,6 +420,29 @@ const gemPuzzle = {
     },
 
 
+    _getPuzzleCoordinates(puzzleId, field) {
+        let coordinates = []
+        let x, y
+
+        for (let i = 0; i < field.length; i++) {
+            // console.log(field[i]);
+
+            for (let j = 0; j < field[i].length; j++) {
+                // console.log(field[i][j]);
+
+                if (field[i][j] === `p${puzzleId}`) {
+
+                    x = j
+                    y = i
+
+                }
+            }
+
+        }
+        coordinates.push(y)
+        coordinates.push(x)
+        return coordinates
+    },
 }
 
 window.addEventListener('DOMContentLoaded', () => {
